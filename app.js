75bbdeb5c96919625df8073c6094f561d341673b
LEONARDO BEZERRA BUSSI <leonardo.bussi@fatec.sp.gov.br>
2020-11-04 16:53:20 -0300

    não funcionou

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-//var helenus = require('helenus');
+var helenus = require('helenus');
 var cassandra = require('cassandra-driver')
 
 var actions = require('./actions');
-// var pool = new helenus.ConnectionPool({
-//  	hosts      : ['localhost:9042'],
-//  	keyspace   : 'disciplinas',
-//  	timeout    : 3000
-//  }); 
+var pool = new helenus.ConnectionPool({ 	hosts      : ['localhost:9042'],
+ 	keyspace   : 'disciplinas',
+	timeout    : 3000
+}); 
 
 var pool = new cassandra.Client({
 	contactPoints: ['h1', 'h2'],
@@ -17,40 +16,40 @@ var pool = new cassandra.Client({
   });
 
 
-// pool.on('error', function(err){
-// 	console.error(err.message);
-// });	
-// var app = express();
-// app.use(express.json());
-// app.use(express.urlencoded());
+pool.on('error', function(err){
+	console.error(err.message);
+});	
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
-// app.get('/css/*', function (req, res) {
-//   res.sendFile(__dirname + '/www/css/' + req.params[0]);
-// });
+app.get('/css/*', function (req, res) {
+res.sendFile(__dirname + '/www/css/' + req.params[0]);
+});
 
-// app.get('/js/*', function (req, res) {
-//   res.sendFile(__dirname + '/www/js/' + req.params[0]);
-// });
+app.get('/js/*', function (req, res) {
+res.sendFile(__dirname + '/www/js/' + req.params[0]);
+});
 
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/www/index.html');
-// });
+app.get('/', function (req, res) {
+ res.sendFile(__dirname + '/www/index.html');
+});
 
-// app.post('/contato', function (req, res) {
-// 	if(typeof(req.body.Atualizardisciplinas) != "undefined"){
-// 		actions.Atualizardisciplinas(req, res, pool);
-// 	} else {
-// 		actions.Inserirdisciplinas(req, res, pool);
-// 	}
-// });
+app.post('/contato', function (req, res) {
+if(typeof(req.body.Atualizardisciplinas) != "undefined"){
+ 		actions.Atualizardisciplinas(req, res, pool);
+ 	} else {
+ 		actions.Inserirdisciplinas(req, res, pool);
+ 	}
+ });
 
-// app.post('/deleta', function (req, res) {	
-// 	actions.Deletadisciplinas(req, res, pool);
-// });
+ app.post('/deleta', function (req, res) {	
+ 	actions.Deletadisciplinas(req, res, pool);
+ });
 
-// app.get('/carrega', function (req, res) {
-// 	actions.Carregadisciplinas(req, res, pool);
-// });
-// var porta = 3000
-// app.listen(porta);
-// console.log('Rodando na porta ', porta);
+ app.get('/carrega', function (req, res) {
+ 	actions.Carregadisciplinas(req, res, pool);
+ });
+ var porta = 3000
+ app.listen(porta);
+ console.log('Rodando na porta ', porta);
